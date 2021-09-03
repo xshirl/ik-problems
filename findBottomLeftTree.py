c953e7eb109af4557248afd2234f0b883d1a3b89
xshirl <xshirl525@gmail.com>
2021-09-03 11:26:09 -0400

    bfs

---
+++ findBottomLeftTree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        res = []
+        queue = deque()
+        queue.append(root)
+        while queue:
+            size =len(queue)
+            level = []
+            for i in range(size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res[-1][0]
---
+++ largestValLevel.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if not root:
+            return []
+        queue = deque()
+        queue.append(root)
+        while queue:
+            size = len(queue)
+            level = []
+            for i in range(size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            maxNode = max(level)
+            res.append(maxNode)
+        return res
